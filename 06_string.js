b38de7644d0db6ce636de0c66723f584f565b063
Debasish-Aditya-Narayan-Pradhan <pradhandebasish5121@gmail.com>
2023-12-18 20:50:09 +0530

    commit

---
+++ 06_string.js
@@ -0,0 +1,20 @@
+const name = "Debasish-19-male-odisha"
+const age = 19
+const gender = "male"
+const state = "Odisha"
+
+//console.log(`My name is: ${name}\nMy age is: ${age}\nMy Gender is: ${gender}\nMy state is: ${state}`);
+
+const a = name.split('-')
+const b = name.toUpperCase();
+const c = name.toLowerCase()
+
+// console.log(b.split('-'))
+// console.log(c.split('-'));
+
+// console.log(name.charAt(4));
+// console.log(name.indexOf('a'));
+
+console.log(name.slice(0,8));
+console.log(name.slice(9,23));
+console.log(name.length);
